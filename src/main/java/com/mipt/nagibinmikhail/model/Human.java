58917c77733300726e0a0f01f5e0de0f57394c4b
MikhailNagibin <nagibimi@gmail.com>
2025-09-22 13:04:15 +0300

    task 3 done

---
+++ src/main/java/com/mipt/nagibinmikhail/model/Human.java
@@ -0,0 +1,21 @@
+package com.mipt.nagibinmikhail.model;
+
+public class Human {
+  private String firstName;
+  private String lastName;
+  private int age;
+  private boolean isWorking;
+
+
+  public String getFirstName() { return firstName; }
+  public void setFirstName(String firstName) { this.firstName = firstName; }
+
+  public String getLastName() { return lastName; }
+  public void setLastName(String lastName) { this.lastName = lastName; }
+
+  public int getAge() { return age; }
+  public void setAge(int age) { this.age = age; }
+
+  public boolean isWorking() { return isWorking; }
+  public void setWorking(boolean working) { isWorking = working; }
+}
