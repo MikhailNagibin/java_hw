af255708d44e79aba70ff681d83fcb2d8d3ed686
MikhailNagibin <nagibimi@gmail.com>
2026-02-26 00:08:06 +0300

    create SpringProject with Spring Initializr

---
+++ src/test/java/com/mipt/nagibinMikhail/toDoList/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mipt.nagibinMikhail.toDoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
