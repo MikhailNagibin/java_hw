5098c6ee4bfc90d9265e3b3354042e515e67befb
MikhailNagibin <nagibimi@gmail.com>
2026-02-26 00:38:03 +0300

    create interface TaskRepository

---
+++ src/main/java/com/mipt/nagibinMikhail/toDoList/repository/TaskRepository.java
@@ -0,0 +1,23 @@
+package com.mipt.nagibinMikhail.toDoList.repository;
+
+import com.mipt.nagibinMikhail.toDoList.Task;
+
+
+/**
+ * интерфейс репозитория для работы с задачами.
+ * Определяет основные CRUD операции.
+ *
+ * createTask(String title, String description, boolean completed) - создание таски. На вход принимает данные о ней
+ *
+ * readTask(int taskId) - получение такси по её id.
+ *
+ * updateTask(String title, String description, boolean completed) - обновление таски. На вход принимает все данные о ней,
+ *
+ * deleteTask(int taskId) - удаление таски по её id
+ */
+public interface TaskRepository {
+    Task createTask(String title, String description, boolean completed);
+    Task readTask(int taskId);
+    Task updateTask(String title, String description, boolean completed);
+    void deleteTask(int taskId);
+}
